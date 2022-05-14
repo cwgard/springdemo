c78f1bdb4437163eb47f5bf48ae182659ca90117
cwgardner@gmail.com <G3thub#1>
2022-05-14 16:14:58 -0400

    added Dependency Injection to StudentController

--- springdemo/src/main/java/com/cw/springdemo/student/StudentController.java
+++ springdemo/src/main/java/com/cw/springdemo/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.cw.springdemo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,15 +10,18 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/student")
+
 public class StudentController {
+
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService){
+        this.studentService = studentService;
+    }
+
     @GetMapping
-    public List<Student> getStudents(){
-        return List.of(
-                new Student(1L,
-                        "Charlie",
-                        "cwgardner@gmail.com",
-                        LocalDate.of(1990, 2, 5),
-                        32)
-        );
+    public List<Student> getStudents() {
+        return studentService.getStudents();
     }
 }
