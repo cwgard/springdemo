6723f74675421c24453c6debe402768b572536eb
cwgardner@gmail.com <G3thub#1>
2022-05-14 16:05:29 -0400

    added RequestMapping to StudentController

--- springdemo/src/main/java/com/cw/springdemo/student/StudentController.java
+++ springdemo/src/main/java/com/cw/springdemo/student/StudentController.java
@@ -1,17 +1,23 @@
 package com.cw.springdemo.student;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "api/v1/student")
 public class StudentController {
     @GetMapping
     public List<Student> getStudents(){
         return List.of(
-                new Student(1L, "Charlie", "cwgardner@gmail.com", LocalDate.of(1990, 2, 5), 32)
+                new Student(1L,
+                        "Charlie",
+                        "cwgardner@gmail.com",
+                        LocalDate.of(1990, 2, 5),
+                        32)
         );
     }
 }
