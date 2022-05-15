667385471733ef1aaede3ea2d3845a04cec8ca7e
cwgardner@gmail.com <G3thub#1>
2022-05-15 10:06:35 -0400

    added missing noargs constructor to Student

--- springdemo/src/main/java/com/cw/springdemo/student/Student.java
+++ springdemo/src/main/java/com/cw/springdemo/student/Student.java
@@ -1,19 +1,33 @@
 package com.cw.springdemo.student;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequnece",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
     private LocalDate dob;
     private int age;
 
-    public Student(Long id,
-                        String name,
-                        String email,
-                        LocalDate dob,
-                        int age) {
+    public Student(
+            Long id,
+            String name,
+            String email,
+            LocalDate dob,
+            int age) {
         this.id = id;
         this.name = name;
         this.email = email;
@@ -21,14 +35,18 @@ public class Student {
         this.age = age;
     }
 
-    public Student(String name,
-                        String email,
-                        LocalDate dob,
-                        int age) {
+    public Student(
+            String name,
+            String email,
+            LocalDate dob,
+            int age) {
         this.name = name;
         this.email = email;
         this.dob = dob;
         this.age = age;
+    }
+
+    public Student() {
     }
 
     public void setId(Long id) {
